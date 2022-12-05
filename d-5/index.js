3cbe41f1425a8853c5724131b3356fb72692a963
Merle Fischer <merle_canada@hotmail.com>
2022-12-05 09:15:03 +0100

    adds part 2 day 5

--- d-5/index.js
+++ d-5/index.js
@@ -1,6 +1,9 @@
 const { testData, data, instructions, testInstructions } = require("./input");
 
-const stackOfBoxes = data;
+///////////////////////////////////
+/////////// Part 1 ///////////////
+/////////////////////////////////
+const stackOfBoxes = JSON.parse(JSON.stringify(data));
 instructions.forEach((instr) => executeMove(instr, stackOfBoxes));
 
 function executeMove(ins, boxStack) {
@@ -15,4 +18,22 @@ let topBoxes = ``;
 for (let prop in stackOfBoxes) {
     topBoxes += stackOfBoxes[prop][0];
 }
-console.log(`Part 1, the top boxes aare ${topBoxes}`);
+console.log(`Part 1, the top boxes are ${topBoxes}`);
+
+///////////////////////////////////
+/////////// Part 2 ///////////////
+/////////////////////////////////
+const stackOfBoxesP2 = JSON.parse(JSON.stringify(data));
+function executeMover9001(ins, boxStack) {
+    const { numsOfBoxes, from, to } = ins;
+
+    const boxSubStack = boxStack[from].splice(0, numsOfBoxes);
+    boxStack[to] = [...boxSubStack, boxStack[to]].flat();
+}
+
+instructions.forEach((instr) => executeMover9001(instr, stackOfBoxesP2));
+let topBoxesP2 = ``;
+for (let prop in stackOfBoxesP2) {
+    topBoxesP2 += stackOfBoxesP2[prop][0];
+}
+console.log(`Part 2, the top boxes are ${topBoxesP2}`);
