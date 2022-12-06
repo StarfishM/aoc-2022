9e39f76c8b2a09e80ab0bc5f54df3fab8dd11b1e
Merle Fischer <merle_canada@hotmail.com>
2022-12-06 09:31:45 +0100

    adds Tuning Trouble d-6 p-2

--- d-6/index.js
+++ d-6/index.js
@@ -1,6 +1,8 @@
 const { testInputs, input } = require("./input");
 // console.log("testInput: ", testInput);
-
+/////////////////////////////
+////////// PART 1 //////////
+///////////////////////////
 //loop through characters
 //check set of fours and go one up
 const checkForMarker = (arr) => {
@@ -13,7 +15,28 @@ const checkForMarker = (arr) => {
 
 // testInputs.forEach((testInp) => console.log(checkForMarker(testInp)));
 console.log(
-    `Part 1: first start-of-packet marker is detected at character ${checkForMarker(
+    `Part 1: first start-of-message marker for 4 unique characters is detected at character ${checkForMarker(
         input
+    )}`
+);
+
+/////////////////////////////
+////////// PART 2 //////////
+///////////////////////////
+const checkForMarkerDynamic = (arr, numOfChars) => {
+    for (let i = 0; i < arr.length; i++) {
+        // generate array of  that need to be added to i
+        const arrayOfIndexes = [...Array(numOfChars).keys()];
+        // generate potential Uniqueset by mapping through those numbers
+        const potentialUniqueSet = arrayOfIndexes.map((idx) => arr[i + idx]);
+        const setOfUnique = [...new Set(potentialUniqueSet)];
+        if (setOfUnique.length === numOfChars) return i + numOfChars;
+    }
+};
+
+console.log(
+    `Part 2: first start-of-message marker for 14 unique characters is detected at character ${checkForMarkerDynamic(
+        input,
+        14
     )}`
 );
