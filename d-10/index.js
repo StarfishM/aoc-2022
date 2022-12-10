ac49d5467c2f6b8a7ad4b6f2fed1783e808b7951
Merle Fischer <merle_canada@hotmail.com>
2022-12-10 11:56:53 +0100

    adds day 10 part 2

--- d-10/index.js
+++ d-10/index.js
@@ -1,5 +1,8 @@
 const { testInput, simpleInput, input } = require("./input");
 // console.log("testInput: ", testInput);
+/////////////////////////////
+////////// PART 1 //////////
+///////////////////////////
 
 const xVals = [];
 const runSignals = (arrOfSignals) => {
@@ -19,6 +22,7 @@ const runSignals = (arrOfSignals) => {
 };
 
 runSignals(input);
+// runSignals(testInput);
 const sumOfSignals = [
     xVals[19] * 20,
     xVals[59] * 60,
@@ -28,4 +32,35 @@ const sumOfSignals = [
     xVals[219] * 220,
 ].reduce((a, b) => a + b);
 
-console.log("Part 1, sum of signals", sumOfSignals);
+console.log("Part 1: sum of signals is:", sumOfSignals);
+
+/////////////////////////////
+////////// PART 2 //////////
+///////////////////////////
+const fst = xVals.slice(0, 39);
+const scd = xVals.slice(40, 79);
+const trd = xVals.slice(80, 119);
+const frth = xVals.slice(120, 159);
+const ffth = xVals.slice(160, 199);
+const sxth = xVals.slice(200, 239);
+
+const returnCRTString = (arrOfSignals) => {
+    let CRTString = "";
+    arrOfSignals.forEach((sigNum, idx) => {
+        if (sigNum <= idx + 1 && sigNum + 1 >= idx) {
+            CRTString += "#";
+        } else {
+            CRTString += ".";
+        }
+    });
+    return CRTString;
+};
+console.log("PART 2 --------------------------------");
+console.log(returnCRTString(fst));
+console.log(returnCRTString(scd));
+console.log(returnCRTString(trd));
+console.log(returnCRTString(frth));
+console.log(returnCRTString(ffth));
+console.log(returnCRTString(sxth));
+console.log("---------------------------------------");
+console.log(`LETTERS ARE: ⭐️ RZHFGJCB ⭐️`);
